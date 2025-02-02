9e41a5c6187b6594e678ef0e8c3ee118bda06492
zapin-live <>
2025-02-02 01:11:13 +0100

    ✨ Add more disable options

--- src/helpers/messenger.ts
+++ src/helpers/messenger.ts
@@ -1,16 +1,35 @@
 export type Message = {
   "activation-toggled": {
     type: "activation-toggled"
+    isActive: boolean
   }
 
-  "set-activation-toggle": {
-    type: "set-activation-toggle",
-    isActive: boolean,
-    description: string
+  "beep": {
+    type: "beep",
+  }
+
+  "disable-until-next-page": {
+    type: "disable-until-next-page",
+  }
+
+  "disable-until": {
+    type: "disable-until",
+    untilMs: number
+  }
+
+  "url-added": {
+    type: "url-added",
+    url: string
+  }
+
+  "url-removed": {
+    type: "url-removed",
+    url: string
   }
 }
 
 export const sendMessageToTab = async <T extends keyof Message>(type: T, message: Omit<Message[T], "type">) => {
+  console.debug("sendMessageToTab", type, message)
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     const tabId = tabs[0].id;
     if (tabId) {
@@ -19,13 +38,10 @@ export const sendMessageToTab = async <T extends keyof Message>(type: T, message
   })
 }
 
-export const sendMessageToUi = async <T extends keyof Message>(type: T, message: Omit<Message[T], "type">) => {
-  chrome.runtime.sendMessage({ type, ...message });
-}
-
-export const receiveMessage = async <T extends keyof Message>(type: T, handler: (message: Message[T]) => void | Promise<void>) => {
+export const receiveMessage = async <T extends keyof Message>(type: T, handler: (message: Message[T]) => void | Promise<void>) => {
   chrome.runtime.onMessage.addListener(async (message) => {
     if (message.type === type) {
+      console.debug("receiveMessage", message)
       handler(message)
     }
   });
