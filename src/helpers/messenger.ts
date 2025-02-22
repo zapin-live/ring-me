78b163a6ea28be768e58ba48c99b1bb7bad3bf97
zapin-live <>
2025-02-22 20:16:44 +0100

    ♻️ Refactor receiveMessage

--- src/helpers/messenger.ts
+++ src/helpers/messenger.ts
@@ -1,54 +1,73 @@
 export type Message = {
   "activation-toggled": {
-    type: "activation-toggled"
-  }
+    type: "activation-toggled";
+  };
 
-  "beep": {
-    type: "beep",
-  }
+  beep: {
+    type: "beep";
+  };
 
   "check-beeper-temporarily-disabled": {
-    type: "check-beeper-temporarily-disabled"
-  }
+    type: "check-beeper-temporarily-disabled";
+  };
 
   "disable-until-next-visit": {
-    type: "disable-until-next-visit",
-  }
+    type: "disable-until-next-visit";
+  };
 
   "disable-until": {
-    type: "disable-until",
-    untilMs: number
-  }
+    type: "disable-until";
+    untilMs: number;
+  };
 
   "url-added": {
-    type: "url-added",
-    url: string
-  }
+    type: "url-added";
+    url: string;
+  };
 
   "url-removed": {
-    type: "url-removed",
-    url: string
-  }
+    type: "url-removed";
+    url: string;
+  };
 
   "volume-changed": {
-    type: "volume-changed",
-    volume: number // 1-100
+    type: "volume-changed";
+    volume: number; // 1-100
+  };
+};
+
+export const sendMessage = async <T extends keyof Message>(
+  type: T,
+  message: Omit<Message[T], "type">,
+) => {
+  console.debug("sendMessage", type, message);
+  return await chrome.runtime.sendMessage({ type, ...message });
+};
+
+const receivers: Record<string, ((message: any) => Promise<unknown>)[]> = {};
+
+export const receiveMessage = async <T extends keyof Message>(
+  type: T,
+  handler: (message: Message[T]) => Promise<unknown>,
+) => {
+  if (!receivers[type]) {
+    receivers[type] = [];
   }
-}
+  receivers[type].push(handler);
+};
 
-export const sendMessage = async <T extends keyof Message>(type: T, message: Omit<Message[T], "type">) => {
-  console.debug("sendMessage", type, message)
-  return await chrome.runtime.sendMessage({ type, ...message })
-}
-
-export const receiveMessage = async <T extends keyof Message>(type: T, handler: (message: Message[T]) => Promise<unknown>) => {
-  chrome.runtime.onMessage.addListener((message, _, sendResponse) => {
-    if (message.type === type) {
-      console.debug("receiveMessage", message)
+chrome.runtime.onMessage.addListener((message, _, sendResponse) => {
+  if (receivers[message.type]) {
+    console.debug(
+      `received Message for ${receivers[message.type].length} subs:`,
+      message,
+    );
+    receivers[message.type].forEach((handler) => {
       handler(message).then((res) => {
-        sendResponse(res)
-      })
-      return true
-    }
-  });
-}
+        sendResponse(res);
+      });
+    });
+
+    return true;
+  }
+});
